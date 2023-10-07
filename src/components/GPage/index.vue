00f6003bda2b915b0a5933c4834cf09ed4755414
殷胜浩 <shenghao.yin@eslink.com>
2023-10-07 14:37:45 +0800

    feat: 新增GPage组件

---
+++ src/components/GPage/index.vue
@@ -0,0 +1,81 @@
+<script setup>
+const props = defineProps({
+	loading: Boolean,
+	search: {
+		type: Object,
+		default: () => {
+			return {
+				model: {},
+				data: [],
+			}
+		},
+	},
+	table: {
+		type: Object,
+		default: () => {
+			return {
+				data: [],
+				columns: [],
+			}
+		},
+	},
+	pagination: {
+		type: Object,
+		default: () => {
+			return {
+				total: 0,
+				pageNum: 1,
+				pageSize: 10,
+			}
+		},
+	},
+})
+const emits = defineEmits(['refresh', 'update:pagination'])
+
+// 搜索条件
+const formRef = ref()
+// 重置
+const handleReset = () => {
+	formRef.value.resetForm()
+	emits('refresh', 1)
+}
+
+// 分页
+const paginationComp = computed({
+	get() {
+		return props.pagination
+	},
+	set(value) {
+		emits('update:pagination', value)
+	},
+})
+</script>
+
+<template lang="pug">
+.page-container(v-loading='loading')
+	//- 搜索调价
+	g-form(v-bind='search', ref='formRef', v-model='search.model', :data='search.data')
+		el-button(v-blur, type='primary', @click='emits("refresh", 1)') 查询
+		el-button(v-blur, @click='handleReset') 重置
+	//- 中间地带
+	slot(name='between')
+	//- 表格
+	g-table(v-bind='$attrs', :columns='table.columns', :data='table.data')
+		template(
+			v-for='column in table.columns',
+			v-show='column.prop',
+			#[column.prop]='{ row, index }'
+		)
+			slot(:name='column.prop', :row='row', :column='column', :index='index')
+	//- 分页
+	g-pagination(v-bind='pagination', v-model='paginationComp', @pagination='emits("refresh")')
+</template>
+
+<style lang="scss" scoped>
+.page-container {
+	display: flex;
+	flex-direction: column;
+	padding: 12px;
+	height: 100%;
+}
+</style>
--- src/views/demo/g-table/index.vue
+++ src/views/demo/g-table/index.vue
@@ -1,71 +1,128 @@
 <script setup>
-const tableRef = ref(null)
-
-const data = ref([
-	{
-		userNo: 111,
-		userName: '用户号1',
-		cardNo: 111,
-	},
-	{
-		userNo: 222,
-		userName: '用户号2',
-		cardNo: 222,
-	},
-	{
-		userNo: 333,
-		userName: '用户号3',
-		cardNo: 333,
-	},
-	{
-		userNo: 444,
-		userName: '用户号4',
-		cardNo: 444,
-	},
-	{
-		userNo: 555,
-		userName: '用户号5',
-		cardNo: 555,
-	},
-])
-const columns = ref([
-	{
-		type: 'selection',
-		width: 60,
-	},
-	{
-		type: 'index',
-		width: 60,
-		label: '序号',
-	},
-	{
-		prop: 'userNo',
-		label: '用户号',
-	},
-	{
-		prop: 'userName',
-		label: '用户名称',
-		columns: [
-			{
-				prop: 'age',
-				label: '年龄',
-			},
-			{
-				prop: 'sex',
-				label: '性别',
-			},
-		],
-	},
-	{
-		prop: 'cardNo',
-		label: '表卡编号',
-	},
-	{
-		prop: 'deal',
-		label: '操作',
-		width: 100,
-	},
-])
+const pageLoading = ref(false)
+// 搜索条件
+const search = ref({
+	model: { name: '', age: 1, sex: '1' },
+	data: [
+		{
+			show: true,
+			key: 'userNo',
+			label: '用户号',
+			component: 'el-input',
+			props: { placeholder: '请输入用户号' },
+		},
+		{
+			show: true,
+			key: 'cardNo',
+			label: '表卡编号',
+			component: 'el-input',
+			props: { placeholder: '请输入表卡编号' },
+		},
+		{
+			show: true,
+			key: 'name',
+			label: '姓名',
+			component: 'el-input',
+			props: { placeholder: '请输入姓名' },
+		},
+		{
+			key: 'age',
+			label: '年龄',
+			component: 'el-input',
+			props: { placeholder: '请输入年龄', clearable: true },
+		},
+		{
+			key: 'sex',
+			label: '性别',
+			component: 'el-select',
+			props: { placeholder: '请选择性别', clearable: true },
+			options: [
+				{
+					label: '男',
+					value: 1,
+				},
+				{
+					label: '女',
+					value: 0,
+				},
+			],
+		},
+	],
+})
+// 表格
+const table = ref({
+	columns: [
+		{
+			type: 'selection',
+			width: 60,
+		},
+		{
+			type: 'index',
+			width: 60,
+			label: '序号',
+		},
+		{
+			prop: 'userNo',
+			label: '用户号',
+		},
+		{
+			prop: 'userName',
+			label: '用户名称',
+			columns: [
+				{
+					prop: 'age',
+					label: '年龄',
+				},
+				{
+					prop: 'sex',
+					label: '性别',
+				},
+			],
+		},
+		{
+			prop: 'cardNo',
+			label: '表卡编号',
+		},
+		{
+			prop: 'deal',
+			label: '操作',
+			width: 100,
+		},
+	],
+	data: [
+		{
+			userNo: 111,
+			userName: '用户号1',
+			cardNo: 111,
+		},
+		{
+			userNo: 222,
+			userName: '用户号2',
+			cardNo: 222,
+		},
+		{
+			userNo: 333,
+			userName: '用户号3',
+			cardNo: 333,
+		},
+		{
+			userNo: 444,
+			userName: '用户号4',
+			cardNo: 444,
+		},
+		{
+			userNo: 555,
+			userName: '用户号5',
+			cardNo: 555,
+		},
+	],
+})
+// 分页
+const page = ref({
+	total: 100,
+	pageNum: 1,
+	pageSize: 10,
+})
 
 const handleSelectionChange = data => {
 	console.log('🚀 ~ file: index.vue:54 ~ handleSelectionChange ~ data:', data)
@@ -74,30 +131,32 @@ const handleEdit = rowData => {
 	console.log('🚀 ~ file: index.vue:76 ~ handleEdit ~ rowData:', rowData)
 }
 
-// 分页
-const total = ref(100)
-const page = ref({
-	pageNum: 1,
-	pageSize: 10,
-})
-const handlePagination = data => {
-	console.log('🚀 ~ file: index.vue:84 ~ handlePagination ~ data:', data)
+const handleGetTableData = async () => {
+	pageLoading.value = true
+	try {
+		setTimeout(() => {
+			pageLoading.value = false
+		}, 1000)
+	} catch (error) {
+		pageLoading.value = false
+	}
 }
 </script>
 
 <template lang="pug">
-.g-form-demo
-	g-table(
-		ref='tableRef',
-		:data='data',
-		:columns='columns',
-		border,
-		stripe,
-		@selection-change='handleSelectionChange'
-	)
-		template(#deal='{ row }')
-			el-button(v-blur, type='primary', size='small', @click='handleEdit(row)') 编辑
-	g-pagination(v-model='page', :total='total', @pagination='handlePagination')
+g-page(
+	:loading='pageLoading',
+	:search='search',
+	:table='table',
+	v-model:pagination='page',
+	@selection-change='handleSelectionChange',
+	@refresh='handleGetTableData'
+)
+	template(#between)
+		.between
+			el-button(v-blur, type='primary') 新增
+	template(#deal='{ row }')
+		el-button(v-blur, type='primary', size='small', @click='handleEdit(row)') 编辑
 </template>
 
 <style lang="scss" scoped>
@@ -106,5 +165,11 @@ const handlePagination = data => {
 	flex-direction: column;
 	padding: 12px;
 	height: 100%;
+}
+.between {
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	margin-bottom: 12px;
 }
 </style>
